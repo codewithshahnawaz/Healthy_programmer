0ce0ab7a6e877d3d4c53230968b786d4a043f2bc
Shahnawaz <shahnawaz28april@gmail.com>
2018-12-20 13:00:32 +0530

    commit

--- Health_programer.py
+++ Health_programer.py
@@ -5,38 +5,38 @@ import time
 def gettime():
 	return (datetime.datetime.now())
 def watr():
-    time.sleep(1800)
-    file = 'water.mp3'
+    time.sleep(10)
+    file = 'Water.mp3'
     mixer.init()
     mixer.music.load(file)
     mixer.music.play(10000,0.0)
-    water = input('Please widte drank to stop the reminder ')
+    water = input('Please write drank to stop the reminder ')
     if water=='Drank' or 'drank':
         mixer.music.pause()
         with open('water.txt','a') as f:
-            f.write(f'{gettime()} Water')
+            f.write(f'{gettime()} Water\n')
 def eydone():
-    time.sleep(4600)
-    eyes = input('Please wite eydone to stop the reminder ')
-    file = 'eyes.mp3'
+    time.sleep(25)
+    eyes = input('Please write eydone to stop the reminder ')
+    file = 'Eyes.mp3'
     mixer.init()
     mixer.music.load(file)
     mixer.music.play(10000,0.0)
     if eyes=='Eydone' or eyes=='eydone':
         mixer.music.pause()
         with open('eyes.txt','a') as f:
-            f.write(f'{gettime()} Eyes Exersice')
+            f.write(f'{gettime()} Eyes Exersice\n')
 def physical():
-    time.sleep(6600)
-    exercise = input('Please wite exdone to stop the reminder ')
-    file = 'physical.mp3'
+    time.sleep(35)
+    exercise = input('Please write exdone to stop the reminder ')
+    file = 'Physical.mp3'
     mixer.init()
     mixer.music.load(file)
     mixer.music.play(10000,0.0)
     if exercise=='Exdone' or exercise=='exdone':
         mixer.music.pause()
-        with open('eyes.txt','a') as f:
-            f.write(f'{gettime()} Physical Exersice')
+        with open('Exercise.txt','a') as f:
+            f.write(f'{gettime()} Physical Exersice\n')
 while True:
     watr()
     eydone()
